4a357b2d533ec68d6d367233108a57d52f8d8a09
Tavonga <67345561+iknowtavonga@users.noreply.github.com>
2022-03-29 19:57:48 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let truck1 = Math.floor(Math.random() * 15) + 1;
+let truck2 = Math.floor(Math.random() * 35) + 1;
+let newHome = 0;
+let i = 0;
 
+console.log(`Truck 2 has ${truck2} boxes`);
+
+while (i > truck1) {
+    newHome++;
+}
+
+console.log(`Truck 1 had ${truck1} boxes and truck 2 had ${truck2} boxes, now your home has ${newHome} boxes`)
